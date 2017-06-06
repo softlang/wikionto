f0b421cd7b41c72b9e097a04c425cb59733b173c
mpaul138 <mpaul@uni-koblenz.de>
2017-06-06 14:14:14 +0200

    text check improvement

--- src/main/java/de/ist/wikionto/research/ArticleChecker.java
+++ src/main/java/de/ist/wikionto/research/ArticleChecker.java
@@ -138,10 +138,14 @@ public class ArticleChecker {
 			System.out.println(td.gov() + " depends on " + td.dep() + " by using " + td.reln());
 		}
 
-		String text = "Ada is a  object-oriented high-level computer programming language, extended from Pascal and other languages. It has built-in language support for design-by-contract, extremely strong typing, explicit concurrency, offering tasks, synchronous message passing, protected objects, and non-determinism. Ada improves code safety and maintainability by using the compiler to find errors in favor of runtime errors. Ada is an international standard; the current version (known as Ada 2012[7]) is defined by ISO/IEC 8652:2012.[8]";
+		String text = "Ada is a structured, statically typed, imperative, wide-spectrum, and object-oriented high-level computer programming language, extended from Pascal and other languages. It has built-in language support for design-by-contract, extremely strong typing, explicit concurrency, offering tasks, synchronous message passing, protected objects, and non-determinism. Ada improves code safety and maintainability by using the compiler to find errors in favor of runtime errors. Ada is an international standard; the current version (known as Ada 2012[7]) is defined by ISO/IEC 8652:2012.";
 		Pattern a = Pattern.compile("\\(.*?\\)");
 		Matcher b = a.matcher(text);
 		text = text.replaceAll("\\(.*?\\)", "");
+		text = text.replace("and", "");
+		text = text.replace("object-oriented,", "");
+		text = text.replace("statically typed,", "");
+		text = text.replace("imperative,", "");
 		System.out.println(checkText(text));
 	}
 
