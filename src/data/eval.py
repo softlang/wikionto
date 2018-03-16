c16a6126a6efc1fc6597cb644788708127ec069e
MarcelH91 <heinz@uni-koblenz.de>
2018-03-16 14:21:48 +0100

    plot cats

--- src/data/eval.py
+++ src/data/eval.py
@@ -35,8 +35,31 @@ def eval_lang_dict():
         
         rsuccess = catalog[(catalog.CLDepth>=0) & (catalog[p]==1)].groupby(by=['CLDepth']).apply(lambda x: len(x))
         rsuccess.to_frame(p+'Succeeds').plot(kind='bar',ax=axes[1,x],color = 'blue', linestyle='dashed')
-        
+    
+    for axar in axes:
+        for ax in axar:
+            for p in ax.patches:
+                ax.annotate(str(p.get_height()), (p.get_x() * 1.005, p.get_height() * 1.005))
     plt.show()
-
+    
+def eval_cat_dict():
+    f= open('catdict.json', 'r',encoding="UTF8")
+    cf = pd.DataFrame(load(f)).transpose()
+    catalog = cf.reindex(columns=["CLDepth","CFFDepth",'NonEmptyCategory','IncludedNamePattern','Eponymous'])
+    
+    fig, axes = plt.subplots(nrows=1, ncols=1)
+    
+    categories = catalog[(catalog.CLDepth>=0)].groupby(by=['CLDepth']).apply(lambda x: len(x)).to_frame('#categories')
+    categories.fillna(0)
+    categories.plot(kind='bar',ax=axes,color = 'blue')
+    cfail = catalog[(catalog.CLDepth>=0) & (catalog.NonEmptyCategory==0)].groupby(by=['CLDepth']).apply(lambda x: len(x)).to_frame('#failCategories')
+    cfail.fillna(0)
+    cfail.plot(kind='bar',ax=axes,color = 'orange')
+    
+    for p in axes.patches:
+        axes.annotate(str(p.get_height()), (p.get_x() * 1.005, p.get_height() * 1.005))
+    plt.show()
+    
 if __name__ == '__main__':
-    eval_lang_dict()
+    #eval_lang_dict()
+    eval_cat_dict()
